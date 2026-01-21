4116c1377ac4fd5b80a03e820f5cae51065e7ce6
Ari <arisaprudin0005@gmail.com>
2026-01-21 21:14:15 +0700

    menambahkan navlink dan error

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import Navbar from '../Navbar/Navbar.jsx';
 import { NavLink, useNavigate } from 'react-router-dom';
+import "./Error.css"
 
 const ErrorPage = () => {
   const navigate = useNavigate();
@@ -8,12 +8,16 @@ const ErrorPage = () => {
   return (
     <div>
       <Navbar />
-      <h3>404 Halaman tidak ditemukan!</h3>
+      <div className='error'>
+      <h1>404 Halaman tidak ditemukan!</h1>
       <p>Oops... Halaman yang kamu cari tidak ada.</p>
 
+    <div className='action'>
       <NavLink to="/">Home</NavLink>
       <br />
       <button onClick={() => navigate(-1)}>Kembali</button>
+      </div>
+      </div>
     </div>
   );
 };
