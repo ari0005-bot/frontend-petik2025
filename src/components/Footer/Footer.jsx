7ba6334370dc65078ce4e4eaacad9d6809adc812
Ari <arisaprudin0005@gmail.com>
2026-02-05 20:44:35 +0700

    gol

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 const Footer = ({ nama }) => {
   const currentYear = new Date().getFullYear();
-  
+
   return (
     <footer className="footer">
       <div className="footer-container">
@@ -14,28 +14,38 @@ const Footer = ({ nama }) => {
               Coffee shop terbaik untuk nongkrong santai bersama teman-teman.
             </p>
           </div>
-          
+
           <div className="footer-section">
             <h5 className="footer-title">Quick Links</h5>
             <ul className="footer-links">
-              <li><a href="/">Home</a></li>
-              <li><a href="/menu">Menu</a></li>
-              <li><a href="/about">About</a></li>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/menu">Menu</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
             </ul>
           </div>
-          
+
           <div className="footer-section">
             <h5 className="footer-title">Contact</h5>
             <p className="footer-contact">
-              Email: info@nongkibroo.com<br />
+              Email: info@nongkibroo.com
+              <br />
               Phone: 0831-7499-7982
             </p>
           </div>
         </div>
-        
+
         <div className="footer-bottom">
           <div className="copyright">
-            <p>&copy; {currentYear} NongkiBroo. Developed with <span className="heart">&#10084;</span> by {nama}</p>
+            <p>
+              &copy; {currentYear} NongkiBroo. Developed with{" "}
+              <span className="heart">&#10084;</span> by {nama}
+            </p>
           </div>
         </div>
       </div>
